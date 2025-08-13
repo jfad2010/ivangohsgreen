2ee0c263ac80c6513d1cf65cfb636b4337f56779
jfad2010 <154806212+jfad2010@users.noreply.github.com>
2025-08-13 16:20:07 +1000

    feat: hash game build and sync

--- game/vite.config.ts
+++ game/vite.config.ts
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: { port: 5173 },
-  build: { outDir: 'dist' }
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]'
+      }
+    }
+  }
 });
